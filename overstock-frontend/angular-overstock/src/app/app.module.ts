cd3188b8278a748d1cad945db2b7384a1ed79817
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-07-01 00:57:11 -0700

    Adding Billing and Shipping Form in checkout component

--- overstock-frontend/angular-overstock/src/app/app.module.ts
+++ overstock-frontend/angular-overstock/src/app/app.module.ts
@@ -31,6 +31,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {CartStatusComponent} from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 const routes: Routes = [
   {path: 'checkout', component: CheckoutComponent},
@@ -64,10 +65,11 @@ const routes: Routes = [
     CheckoutComponent
   ],
   imports: [
+    ReactiveFormsModule,
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
   ],
   providers: [
     WebinarRatingService,
